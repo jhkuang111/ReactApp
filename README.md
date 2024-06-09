b9fffbdefe99f6bf1e51224f4152fe0c5b9d779b
jhkuang111 <jhkuang11@yahoo.com>
2024-06-09 15:54:29 -0700

    Minor update for README

--- README.md
+++ README.md
@@ -2,10 +2,9 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Step 1
+If there is a change of package versions, delete `node_modules` and `package-lock.json` and run `npm install` to install followed package versions before developing.
 
-Delete `node_modules` and `package-lock.json` and run `npm install` to install followed package versions
-
+## Run locally
 
 Runs `npm start` to get the server up and running.\
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import './styles.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import MoviesGrid from './components/MoviesGrid';
+import "./App.css";
+import "./styles.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MoviesGrid from "./components/MoviesGrid";
 
 function App() {
   return (
     <div className="App">
-      <div className='container'>
+      <div className="container">
         <Header></Header>
         <MoviesGrid></MoviesGrid>
       </div>
-      
+
       <Footer></Footer>
     </div>
   );
