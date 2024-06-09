3dba7beea58e6fb3f3bf8553810e1f8c3bd98ddd
jhkuang111 <jhkuang11@yahoo.com>
2024-06-08 18:36:28 -0700

    Create Header component and import it to the App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import './styles.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        <h1>Welcome to Moviedux</h1>
-      </header>
+      <div className='container'>
+        <Header></Header>
+      </div>
       <footer className="footer">
         <p className='footer'>Footer content here</p>
       </footer>
